e3470ec2115d188841cc9f9a7238b93d7db49a0a
Brian Pak <brianpak27@gmail.com>
2018-01-26 15:08:46 -0800

    added more tests

--- waterwall.test.js
+++ waterwall.test.js
@@ -20,7 +20,14 @@ test ('should find no water when walls consistently decrease in size', () => {
   expect(water.waterWall([6, 5, 4, 3, 2, 1])).toEqual([]);
 });
 
+test ('should find no water when walls consistently increase in size', () => {
+  expect(water.waterWall([1, 2, 3, 4, 5, 6])).toEqual([]);
+});
+
 test('should return the first set of walls in case of ties', () => {
   expect(water.waterWall([3, 1, 3, 1, 3])).toEqual([1, 3, 2]);
 });
 
+test('finds 13 water between walls 4 and 8', () => {
+  expect(water.waterWall([7, 1, 1, 7, 5, 2, 1, 7])).toEqual([4, 8, 13]);
+});
