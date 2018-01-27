8251566f744d067e8fa8e4e5d5a1f59c7fdb3830
Brian Pak <brianpak27@gmail.com>
2018-01-26 17:25:44 -0800

    server route added

--- server/index.js
+++ server/index.js
@@ -4,7 +4,13 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
+app.post('/water', (req, res) => {
+  let heights = req.body.heights;
+  res.json(water.waterwall(heights));
+})
 
 app.listen(port, () => {
   console.log(`listening on port: ${port}`);
